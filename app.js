881e620af4962eeb99d88225122c945d7f33f913
hernandw <hernandw@gmail.com>
2022-01-18 16:16:22 -0300

    creacion de routes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 require('dotenv').config({path: '.env'});
 const PORT = process.env.PORT || 5000;
 
+
+app.use(require('./routes/index'));
+
+
+//Levantar Servidor
 app.listen(PORT, ()=>{
     console.log(`Servidor activo en puerto ${PORT}`);
 })
