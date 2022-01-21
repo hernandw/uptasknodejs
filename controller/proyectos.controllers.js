8adf1b09367e8f8487d10f0d714fed7bac71440d
hernandw <hernandw@gmail.com>
2022-01-21 13:48:26 -0300

    modificación de proyectos y mixin

--- controller/proyectos.controllers.js
+++ controller/proyectos.controllers.js
@@ -1,5 +1,6 @@
 const proyectos = require('../models/project')
-const slug = require('slug')
+const slug = require('slug');
+
 
 exports.home = async (req, res) => {
     const projects = await proyectos.findAll();
@@ -59,9 +60,62 @@ exports.proyectosPorUrl = async (req, res, next) =>{
     if(!proyecto){
         return next()
     }
-    res.render('Tareas', {
+    
+    res.render('tareas', {
         nombrePagina: 'Tareas del Proyecto',
-        proyecto,
-        projects
+        projects,
+        proyecto
     })
 }
+
+exports.formularioEditar = async (req, res) => {
+  const projectsPromise = proyectos.findAll();
+  const proyectoPromise = proyectos.findOne({
+    where: {
+        id: req.params.id
+    }
+ });
+
+ const [projects, proyecto] = await Promise.all([projectsPromise, proyectoPromise])
+  res.render('nuevoproyecto', {
+    nombrePagina: 'Editar Proyecto',
+    projects,
+    proyecto
+  })
+}
+
+exports.updateProyecto = async (req, res) => {
+  const projects = await proyectos.findAll();
+const { name } = req.body;
+
+// Valida que el input no esté vacio
+let errores = [];
+
+if (!name) {
+  errores.push({ texto: " Agrega un nombre al proyecto" });
+}
+
+//Si hay errores
+if (errores.length > 0) {
+  res.render("nuevoProyecto", {
+    nombrePagina: "Nuevo Proyecto",
+    errores,
+    projects
+  });
+} else {
+  //Inserta registro en la BBDD
+  try {
+      const url = slug(name).toLowerCase().trim()
+    const project = await proyectos.update({ name, url },{
+      where: {
+        id: req.params.id
+      }
+    }
+      );
+    
+  } catch (error) {
+    console.log("Error al guardar los datos " + error);
+  } 
+  res.redirect("/");
+}
+};
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/nuevo-proyecto', proyectos.formulario);
 
 router.post('/nuevo-proyecto', proyectos.nuevoProyecto);
 
-router.get('/:url', proyectos.proyectosPorUrl);
+router.get('/editar/:url', proyectos.proyectosPorUrl);
+
+router.get('/editar/:id', proyectos.formularioEditar)
+
+router.post('/nuevo-proyecto/:id', proyectos.updateProyecto)
 
 module.exports = router
